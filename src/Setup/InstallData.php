d1c30028fc455a842fb36b09e0247e1bae580f05
Lucas van Staden <sales@proxiblue.com.au>
2021-05-26 04:18:59 +0000

    silent release for client usage

--- src/Setup/InstallData.php
+++ src/Setup/InstallData.php
@@ -1,4 +1,13 @@
 <?php
+
+/*
+ * (c) Lucas van Staden <sales@proxiblue.com.au>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
 namespace ProxiBlue\MultiMenu\Setup;
 
 use Magento\Eav\Setup\EavSetupFactory;
--- src/ViewModel/Navigation.php
+++ src/ViewModel/Navigation.php
@@ -6,13 +6,24 @@ namespace ProXiBlue\MultiMenu\ViewModel;
 
 use Hyva\Theme\ViewModel\Navigation as HyvaNavigation;
 use ProxiBlue\MultiMenu\Service\Navigation as NavigationService;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Navigation extends HyvaNavigation
 {
 
-    public function __construct(NavigationService $navigationService)
+    protected $scopeConfig;
+
+    const XML_PATH_MULTIMENU = 'proxiblue_multimenu/';
+
+    public function __construct(NavigationService $navigationService, ScopeConfigInterface $scopeInterface)
     {
         parent::__construct($navigationService);
+        $this->scopeConfig = $scopeInterface;
+    }
+
+    public function getCategoryDepth($storeId = null)
+    {
+        return $this->scopeConfig->getValue(self::XML_PATH_MULTIMENU . 'general/depth', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
 
 }
