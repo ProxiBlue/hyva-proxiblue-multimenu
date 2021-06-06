a5d19a4de7030e7f1be59501cc50222e2422c600
Lucas van Staden <sales@proxiblue.com.au>
2021-06-07 06:34:05 +0800

    Add copyright

--- src/ViewModel/Image.php
+++ src/ViewModel/Image.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Lucas van Staden <sales@proxiblue.com.au>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 declare(strict_types=1);
 
 namespace ProXiBlue\MultiMenu\ViewModel;
