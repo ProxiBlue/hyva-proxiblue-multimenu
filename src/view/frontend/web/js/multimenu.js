343bc69a36b7ffb05c4f1c4f5cf4dfbc1a3912f0
Lucas <lucas.vanstaden@enjo.com>
2022-01-20 03:30:10 +0000

    adjust to remove duplicated js code. Fix issue iwth mobile needing double click to open

---
+++ src/view/frontend/web/js/multimenu.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const initMenu = () => {
+    return {
+        mobilePanelActiveId: null,
+        hoverPanelActiveId: null,
+        open: false,
+        setActiveMenu(menuNode) {
+            Array.from(menuNode.querySelectorAll('a')).filter(link => {
+                return link.href === window.location.href.split('?')[0];
+            }).map(item => {
+                item.classList.add('underline');
+                item.closest('div.level-0') &&
+                item.closest('div.level-0').querySelector('a.level-0').classList.add('underline');
+            });
+        },
+        checkSlideDirection(event) {
+            let domElm = event.target
+            if (domElm.nextElementSibling) {
+                let UlElm = domElm.nextElementSibling;
+                return new Promise((resolve, reject) => {
+                    UlElm.classList.remove('slide-to-left');
+                    UlElm.classList.remove('slide-to-right')
+                    return resolve();
+                })
+                    .then(() => {
+                        if ((domElm.getBoundingClientRect().right + UlElm.offsetWidth) > window.innerWidth) {
+                            UlElm.style.zIndex = -1;
+                            UlElm.classList.add('slide-to-left')
+                            this.slideToLeft = true;
+                        } else {
+                            UlElm.classList.add('slide-to-right');
+                            this.slideToLeft = false;
+                        }
+                    });
+
+            }
+        },
+        handleMobileOpenMenu(event) {
+            if (event.target.localName == 'svg') {
+                var domElm = event.target.parentElement.parentElement;
+            } else {
+                var domElm = event.target.parentElement.parentElement.parentElement;
+            }
+            if (domElm.nextElementSibling) {
+                let UlElm = domElm.nextElementSibling;
+                let parent = domElm.offsetParent
+                if (parent.classList.contains('menu-active')) {
+                    parent.classList.remove('menu-active');
+                    domElm.classList.remove('menu-active');
+                    UlElm.classList.remove('open-down');
+                } else {
+                    parent.classList.add('menu-active');
+                    domElm.classList.add('menu-active');
+                    UlElm.classList.add('open-down');
+                }
+
+            }
+
+        },
+        moveMenuInDom() {
+            if (this.open === true) {
+                this.manipulateDOM('nav-mobile-container', 'nav-desktop-container')
+                document.getElementById('html-body').classList.add('fixed-position')
+                document.getElementById('nav-mobile-scroll').classList.add('mobile-scroll')
+            } else {
+                this.manipulateDOM('nav-desktop-container', 'nav-mobile-container')
+                document.getElementById('html-body').classList.remove('fixed-position')
+                document.getElementById('nav-mobile-scroll').classList.remove('mobile-scroll')
+            }
+        },
+        manipulateDOM(to, from) {
+            var NavElm = document.getElementById(to);
+            if (NavElm.innerHTML.trim() == '') {
+                let srcElm = document.getElementById(from);
+                return new Promise((resolve, reject) => {
+                    NavElm.append(...srcElm.childNodes);
+                    return resolve();
+                })
+                    .then(() => {
+                        Alpine.discoverUninitializedComponents(function (el) {
+                            Alpine.initializeComponent(el)
+                        })
+                    });
+            }
+        }
+    }
+}
+
